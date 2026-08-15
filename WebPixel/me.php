c6d285397cf8a4da0ee4e60e0c37aa365a426696
TrashrProject <77882564+TrashrProject@users.noreply.github.com>
2026-08-15 18:09:56 +0200

    Fix localhost me redirect

--- WebPixel/me.php
+++ WebPixel/me.php
@@ -1,25 +1,18 @@
 <?php
 /**
  * PixelZone by RDP Services, Emulated by Retro Development Server.
- * The use of this program is restricted to clients and owners of RDP Services.
- * Any unauthorized use of this code it'll end up on deletion of the program.
- * Developers P3x & Jeihden.
- * Copyrights © 2020
- * Last Modified: $file.lastModefied
  */
 
 require_once "app/init.pz.php";
 
-// Redirect if logged in
+// Redirect if not logged in
 if(!$Session->Exist(Config::$SessionName)):
-    header("Location: /");
+    header("Location: " . Config::$URL . "/");
     exit;
 endif;
 
-//Page Name
 $PageName = "Me";
 
-// Load Pages
 require_once HEADER . 'main.php';
 require_once NAVBAR . 'navbar.php';
 require_once BODY . 'me.php';
