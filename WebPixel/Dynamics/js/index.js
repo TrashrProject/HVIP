2ccbf94bf898f4f33787e45b9b51172282a77437
TrashrProject <77882564+TrashrProject@users.noreply.github.com>
2026-08-15 18:09:22 +0200

    Fix localhost login and registration routes

--- WebPixel/Dynamics/js/index.js
+++ WebPixel/Dynamics/js/index.js
@@ -1,183 +1,129 @@
-/*
- * PixelZone by RDP Services, Emulated by Retro Development Server.
- * The use of this program is restricted to clients and owners of RDP Services.
- * Any unauthorized use of this code it'll end up on deletion of the program.
- * Developers P3x & Jeihden.
- * Copyrights © 2020
- * Last Modified: $file.lastModefied
- */
+/* Localhost-safe login / registration for WebPixel */
 
+console.log("[Init] -> Inicializando sistema de login");
 
-console.log("%c      ################################################################################################", 'color: #daddef;text-shadow: 1px 1px 1px rgba(29, 44, 154, 0.8), -1px -1px 1px rgba(33, 52, 187, 0.8), 1px -1px 1px rgba(28, 51, 162, 0.8), -1px 1px 1px rgba(16, 42, 169, 0.8);');
-console.log("%c      #                                                                                              #", 'color: #daddef;text-shadow: 1px 1px 1px rgba(29, 44, 154, 0.8), -1px -1px 1px rgba(33, 52, 187, 0.8), 1px -1px 1px rgba(28, 51, 162, 0.8), -1px 1px 1px rgba(16, 42, 169, 0.8);');
-console.log("%c      #  ##########################################################################################  #", 'color: #daddef;text-shadow: 1px 1px 1px rgba(29, 44, 154, 0.8), -1px -1px 1px rgba(33, 52, 187, 0.8), 1px -1px 1px rgba(28, 51, 162, 0.8), -1px 1px 1px rgba(16, 42, 169, 0.8);');
-console.log("%c      #  #,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,#  #", 'color: #daddef;text-shadow: 1px 1px 1px rgba(29, 44, 154, 0.8), -1px -1px 1px rgba(33, 52, 187, 0.8), 1px -1px 1px rgba(28, 51, 162, 0.8), -1px 1px 1px rgba(16, 42, 169, 0.8);');
-console.log("%c      #  #,,_ _ _ _ ,,,,,_________,,,,, _ _ _ _ ,,,,,___________,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,#  #", 'color: #daddef;text-shadow: 1px 1px 1px rgba(29, 44, 154, 0.8), -1px -1px 1px rgba(33, 52, 187, 0.8), 1px -1px 1px rgba(28, 51, 162, 0.8), -1px 1px 1px rgba(16, 42, 169, 0.8);');
-console.log("%c      #  #,|   ___  \\  ,|   ____  \\   ,|   ___  \\  ,|  _________|,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,#  #", 'color: #daddef;text-shadow: 1px 1px 1px rgba(29, 44, 154, 0.8), -1px -1px 1px rgba(33, 52, 187, 0.8), 1px -1px 1px rgba(28, 51, 162, 0.8), -1px 1px 1px rgba(16, 42, 169, 0.8);');
-console.log("%c      #  #,|  |   \\  \\ ,|  |    \\  \\  ,|  |   \\  \\ ,|  |,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,#  #", 'color: #daddef;text-shadow: 1px 1px 1px rgba(29, 44, 154, 0.8), -1px -1px 1px rgba(33, 52, 187, 0.8), 1px -1px 1px rgba(28, 51, 162, 0.8), -1px 1px 1px rgba(16, 42, 169, 0.8);');
-console.log("%c      #  #,|  |    \\  \\,|  |     \\  \\ ,|  |    \\  \\,|  |,,,,,,,,,,,_____,,,,,,,_____,,_,,,,,,,,_,,#  #", 'color: #daddef;text-shadow: 1px 1px 1px rgba(29, 44, 154, 0.8), -1px -1px 1px rgba(33, 52, 187, 0.8), 1px -1px 1px rgba(28, 51, 162, 0.8), -1px 1px 1px rgba(16, 42, 169, 0.8);');
-console.log("%c      #  #,|  |____/  /,|  |      \\  @,|  |____/  /,|  |________ ,|  __ \\,,,,,/ __  || |      | | #  #", 'color: #daddef;text-shadow: 1px 1px 1px rgba(29, 44, 154, 0.8), -1px -1px 1px rgba(33, 52, 187, 0.8), 1px -1px 1px rgba(28, 51, 162, 0.8), -1px 1px 1px rgba(16, 42, 169, 0.8);');
-console.log("%c      #  #,|   _   __/ ,|  |      @  @,|   ______/ ,|   ________|,| |,,\\ \\,,,/ /,,| || |      | | #  #", 'color: #daddef;text-shadow: 1px 1px 1px rgba(29, 44, 154, 0.8), -1px -1px 1px rgba(33, 52, 187, 0.8), 1px -1px 1px rgba(28, 51, 162, 0.8), -1px 1px 1px rgba(16, 42, 169, 0.8);');
-console.log("%c      #  #,|  | \\  \\   ,|  |      /  @,|  |        ,|  |         ,| |,,,\\ \\_/ /,,,| || |      | | #  #", 'color: #daddef;text-shadow: 1px 1px 1px rgba(29, 44, 154, 0.8), -1px -1px 1px rgba(33, 52, 187, 0.8), 1px -1px 1px rgba(28, 51, 162, 0.8), -1px 1px 1px rgba(16, 42, 169, 0.8);');
-console.log("%c      #  #,|  |  \\  \\  ,|  |     /  / ,|  |        ,|  |         ,| |,,,,\\___/,,,,| || |      | | #  #", 'color: #daddef;text-shadow: 1px 1px 1px rgba(29, 44, 154, 0.8), -1px -1px 1px rgba(33, 52, 187, 0.8), 1px -1px 1px rgba(28, 51, 162, 0.8), -1px 1px 1px rgba(16, 42, 169, 0.8);');
-console.log("%c      #  #,|  |   \\  \\ ,|  |____/  /  ,|  |        ,|  |________ ,| |,,,,,,,,,,,,,| |\\ \\_____ / / #  #", 'color: #daddef;text-shadow: 1px 1px 1px rgba(29, 44, 154, 0.8), -1px -1px 1px rgba(33, 52, 187, 0.8), 1px -1px 1px rgba(28, 51, 162, 0.8), -1px 1px 1px rgba(16, 42, 169, 0.8);');
-console.log("%c      #  #,|__|    \\__\\,|_________/   ,|__|        ,|___________|,|_|,,,,,,,,,,,,,|_|,\\________/,,#  #", 'color: #daddef;text-shadow: 1px 1px 1px rgba(29, 44, 154, 0.8), -1px -1px 1px rgba(33, 52, 187, 0.8), 1px -1px 1px rgba(28, 51, 162, 0.8), -1px 1px 1px rgba(16, 42, 169, 0.8);');
-console.log("%c      #  #,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,V:1.0.0.1,,,#  #", 'color: #daddef;text-shadow: 1px 1px 1px rgba(29, 44, 154, 0.8), -1px -1px 1px rgba(33, 52, 187, 0.8), 1px -1px 1px rgba(28, 51, 162, 0.8), -1px 1px 1px rgba(16, 42, 169, 0.8);');
-console.log("%c      #  #,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,#  #", 'color: #daddef;text-shadow: 1px 1px 1px rgba(29, 44, 154, 0.8), -1px -1px 1px rgba(33, 52, 187, 0.8), 1px -1px 1px rgba(28, 51, 162, 0.8), -1px 1px 1px rgba(16, 42, 169, 0.8);');
-console.log("%c      #  ##########################################################################################  #", 'color: #daddef;text-shadow: 1px 1px 1px rgba(29, 44, 154, 0.8), -1px -1px 1px rgba(33, 52, 187, 0.8), 1px -1px 1px rgba(28, 51, 162, 0.8), -1px 1px 1px rgba(16, 42, 169, 0.8);');
-console.log("%c      #  Licensed To: PZN2020                                                        Jeihden & Zedd  #", 'color: #daddef;text-shadow: 1px 1px 1px rgba(29, 44, 154, 0.8), -1px -1px 1px rgba(33, 52, 187, 0.8), 1px -1px 1px rgba(28, 51, 162, 0.8), -1px 1px 1px rgba(16, 42, 169, 0.8);');
-console.log("%c      ################################################################################################", 'color: #daddef;text-shadow: 1px 1px 1px rgba(29, 44, 154, 0.8), -1px -1px 1px rgba(33, 52, 187, 0.8), 1px -1px 1px rgba(28, 51, 162, 0.8), -1px 1px 1px rgba(16, 42, 169, 0.8);');
-console.log("");
-console.log("");
+(function ($) {
+    'use strict';
 
-console.log("[Init] -> Inicializando systema de login");
+    var baseUrl = window.location.pathname;
+    if (!baseUrl.endsWith('/')) {
+        baseUrl = baseUrl.substring(0, baseUrl.lastIndexOf('/') + 1);
+    }
+    var endpoint = baseUrl;
 
-// Login - register switch
-$("#show-register" ).click(function() {
-    $( "#login-box" ).hide( "fast");
-    $( "#register-box" ).show("fast");
-});
-
-$("#show-login" ).click(function() {
-    $( "#register-box" ).hide("fast");
-    $( "#login-box" ).show( "fast");
-});
-
-// Login JS
-$(document).ready(function(){
-    $("#subrmit-login").click(function(){
-        Login();
-    });
-
-    $("#pz-login-uname").on('keypress',function(e) {
-        if(e.which == 13) {
-            Login();
+    function parseResponse(data) {
+        if (typeof data === 'object') return data;
+        try {
+            return JSON.parse(data);
+        } catch (e) {
+            console.error('[RDP] Réponse PHP invalide:', data);
+            return { result: false, msg: 'Le serveur a renvoyé une réponse invalide.' };
         }
+    }
+
+    function showError(box, msgBox, message) {
+        $(msgBox).text(message);
+        $(box).stop(true, true).show('fast');
+    }
+
+    $('#show-register').on('click', function () {
+        $('#login-box').hide('fast');
+        $('#register-box').show('fast');
     });
 
-    $("#pz-login-pass").on('keypress',function(e) {
-        if(e.which == 13) {
-            Login();
-        }
+    $('#show-login').on('click', function () {
+        $('#register-box').hide('fast');
+        $('#login-box').show('fast');
     });
-
-    // Evento Enter
-    // Login();
 
     function Login() {
-        var uname = $("#pz-login-uname").val();
-        var pass = $("#pz-login-pass").val();
-        if( uname != "" && pass != "" ){
-            $.post("/",
-                {
-                    login_username: '' + uname,
-                    login_password: '' + pass
-                },
-                function(data){
-                     var res = $.parseJSON(data);
-                    if(res.result == true){
-                        $("#e-login-message" ).hide("slow");
-                        $("#login-message" ).show("slow");
-                        $("#login-msg").html(res.msg);
-                        console.log("[RDP] -> Usuario [" + uname + "] ha iniciado sesión correctamente, redireccionando en 3 segundos...");
-                        setTimeout(function(){ window.location = "/me"; }, 1);
-                    } else {
-                        $("#e-login-message" ).hide("slow");
-                        $("#e-login-msg").html(res.msg);
-                        $("#e-login-message" ).show("slow");
-                        console.log("[RDP] -> " + res.msg);
-                    }
-                });
-        } else {
-            $("#e-login-message" ).show("slow");
-            $("#e-login-msg").html("¡Debes llenar todos los campos!");
+        var username = $('#pz-login-uname').val().trim();
+        var password = $('#pz-login-pass').val();
+
+        $('#e-login-message').hide();
+
+        if (!username || !password) {
+            showError('#e-login-message', '#e-login-msg', '¡Debes llenar todos los campos!');
+            return;
         }
+
+        $.ajax({
+            url: endpoint,
+            method: 'POST',
+            data: {
+                login_username: username,
+                login_password: password
+            }
+        }).done(function (data) {
+            var res = parseResponse(data);
+            if (res.result === true) {
+                $('#login-msg').text(res.msg || 'Connexion réussie.');
+                $('#login-message').show('fast');
+                window.location.href = baseUrl + 'me.php';
+            } else {
+                showError('#e-login-message', '#e-login-msg', res.msg || 'Connexion impossible.');
+            }
+        }).fail(function (xhr) {
+            console.error('[RDP] Login HTTP error', xhr.status, xhr.responseText);
+            showError('#e-login-message', '#e-login-msg', 'Erreur HTTP ' + xhr.status + ' pendant la connexion.');
+        });
     }
-});
-
-
-// Register JS
-$(document).ready(function(){
-    $("#subrmit-register").click(function(){
-        Register();
-    });
-    $("#register-username").on('keypress',function(e) {
-        if(e.which == 13) {
-            Register();
-        }
-    });
-
-    $("#register-password").on('keypress',function(e) {
-        if(e.which == 13) {
-            Register();
-        }
-    });
-
-    $("#register-password-confirm").on('keypress',function(e) {
-        if(e.which == 13) {
-            Register();
-        }
-    });
-
-    $("#email").on('keypress',function(e) {
-        if(e.which == 13) {
-            Register();
-        }
-    });
 
     function Register() {
+        var username = $('#register-username').val().trim();
+        var password = $('#register-password').val();
+        var confirm = $('#register-password-confirm').val();
+        var email = $('#email').val().trim();
 
-        var username = $("#register-username").val();
-        var password = $("#register-password").val();
-        var password_confirm = $('#register-password-confirm').val();
-        var email = $("#email").val();
+        $('#e-register-message').hide();
 
-        $("#e-login-message" ).hide( "slow");
-        $("#e-register-message" ).hide( "slow");
-
-        if( username != "" && password != "" && password_confirm != "" && email != ""){
-            if(password == password_confirm){
-                $.post("/",
-                    {
-                        reg_username: '' + username,
-                        reg_password: '' + password,
-                        reg_mail: '' + email
-                    },
-                    function(data){
-                        var res = $.parseJSON(data);
-                        if(res.result == true){
-                            $("#e-register-message" ).hide( "slow");
-                            $("#register-message" ).show( "slow");
-                            $("#register-msg").html(res.msg);
-                            console.log("[RDP] -> Usuario [" + username + "] se ha registrado correctamente, redireccionando en 3 segundos...");
-                            setTimeout(function(){ window.location = "/me?newuser=true"; }, 2000);
-                        } else {
-                            $("#e-register-message" ).show( "slow");
-                            $("#e-register-msg").html(res.msg);
-                            console.log("[RDP] -> " + res.msg);
-                        }
-                    });
-            } else {
-                $("#e-register-message" ).show( "slow");
-                $("#e-register-msg").html("Tus contraseñas no coinciden.");
-            }
-
-        } else {
-            $("#e-register-message" ).show("slow");
-            $("#e-register-msg").html("¡Debes llenar todos los campos!");
+        if (!username || !password || !confirm || !email) {
+            showError('#e-register-message', '#e-register-msg', '¡Debes llenar todos los campos!');
+            return;
         }
 
+        if (password !== confirm) {
+            showError('#e-register-message', '#e-register-msg', 'Tus contraseñas no coinciden.');
+            return;
+        }
+
+        $.ajax({
+            url: endpoint,
+            method: 'POST',
+            data: {
+                reg_username: username,
+                reg_password: password,
+                reg_mail: email
+            }
+        }).done(function (data) {
+            var res = parseResponse(data);
+            if (res.result === true) {
+                $('#register-msg').text(res.msg || 'Compte créé.');
+                $('#register-message').show('fast');
+                setTimeout(function () {
+                    window.location.href = baseUrl + 'me.php?newuser=true';
+                }, 700);
+            } else {
+                showError('#e-register-message', '#e-register-msg', res.msg || 'Inscription impossible.');
+            }
+        }).fail(function (xhr) {
+            console.error('[RDP] Register HTTP error', xhr.status, xhr.responseText);
+            showError('#e-register-message', '#e-register-msg', 'Erreur HTTP ' + xhr.status + ' pendant l’inscription.');
+        });
     }
-});
 
-// Facebook Login
-$(document).ready(function(){
-    $("#fb-login").click(function(){
-        var left = (screen.width ) / 3 ;
-        var top = (screen.height ) / 5 ;
-        let params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,
-        width=600,height=600,left= ` + left + ` ,top=` + top + ``;
-        window.open('https://www.facebook.com/v7.0/dialog/oauth?client_id=2991567804406685&redirect_uri=https://habbovip.us/fb_login&auth_type=rerequest&scope=email', '', params);
+    $(document).ready(function () {
+        $('#subrmit-login').on('click', Login);
+        $('#subrmit-register').on('click', Register);
+
+        $('#pz-login-uname, #pz-login-pass').on('keypress', function (e) {
+            if (e.which === 13) Login();
+        });
+
+        $('#register-username, #register-password, #register-password-confirm, #email').on('keypress', function (e) {
+            if (e.which === 13) Register();
+        });
+
+        $('#fb-login').hide();
     });
-});
-
+})(jQuery);
