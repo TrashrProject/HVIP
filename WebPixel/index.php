da6b8e867511c05cf58df82d5b78087cab725b63
TrashrProject <77882564+TrashrProject@users.noreply.github.com>
2026-08-15 18:09:47 +0200

    Fix localhost index redirect

--- WebPixel/index.php
+++ WebPixel/index.php
@@ -1,21 +1,15 @@
 <?php
 /**
  * PixelZone by RDP Services, Emulated by Retro Development Server.
- * The use of this program is restricted to clients and owners of RDP Services.
- * Any unauthorized use of this code it'll end up on deletion of the program.
- * Developers P3x & Jeihden.
- * Copyrights © 2020
- * Last Modified: $file.lastModefied
  */
 
 require_once "app/init.pz.php";
 
 // Redirect if logged in
 if($Session->Exist(Config::$SessionName)):
-    header("Location: /me");
+    header("Location: " . Config::$URL . "/me.php");
     exit;
 endif;
 
-// Load Pages
 require_once HEADER . 'index.php';
 require_once BODY . 'index.php';
