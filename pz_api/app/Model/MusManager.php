fb3397ca65fcf19a23cab7a09030ce35f985ca45
TrashrProject <77882564+TrashrProject@users.noreply.github.com>
2026-08-15 17:32:03 +0200

    Configure MUS socket for localhost and improve socket errors

--- pz_api/app/Model/MusManager.php
+++ pz_api/app/Model/MusManager.php
@@ -1,74 +1,89 @@
 <?php
 /**
- * This API Back-end system was design and developed for RDP Services.
- * Developed by P3x & Jeihden.
- * All rights reserved PZ API Copyright (c) 2020.
- *
+ * PZ API MUS socket manager.
+ * Local emulator defaults: 127.0.0.1:3043.
  */
 
 class MusManager
 {
-    // Config MUS
-    protected static $H = "51.222.208.249";      // Host IP
-    protected static $P = "3043";               // Host Port
+    protected static $H = '127.0.0.1';
+    protected static $P = 3043;
 
-    // DO NOT TOUCH DYNAMIC VARIABLES
-    protected $Host;      // Host IP
-    protected $Port;      // Host Port
+    protected $Host;
+    protected $Port;
 
-    ## SENDING PACKAGES FUNCTIONS ##
-    // Connects to Socket
-    protected function Con(){
+    protected function Con()
+    {
+        $this->Host = self::$H;
+        $this->Port = self::$P;
 
-        $this->Host = self::$H;       // IP
-        $this->Port = self::$P;       // Port
-
-        $S = socket_create(AF_INET, SOCK_STREAM, SOL_TCP) or die("Could not create socket\n");
-        $R = socket_connect($S, $this->Host, $this->Port) or die("Could not connect to server2\n");
-
-        return $S;
-    }
-
-    // Set The Package ready
-    public function Ready($C, $P = null){
-        // Start Building the String Socket to Send
-
-        // Command is added to the string
-        $S = $C . ":";
-
-        // If Params aren't null we add params to the string
-        if($P != null):
-            $Count = 0;
-            // Foreach to get all the params required
-            foreach ($P as $K => $V):
-                if($Count >= 1):
-                    $S .= "|". $V;
-                else:
-                    $S .= $V;
-                endif;
-                $Count++;
-            endforeach;
-        endif;
-        
-        // Once String is completed send the package
-        $this->Send($S);
-    }
-
-    // Sends the Package with Socket
-    public function Send($S, $return = false){
-        $Socket = $this->Con();
-
-        socket_write($Socket, $S, strlen($S)) or die("Could not send data to server\n");
-        $R = null;
-        while ($out = socket_read($Socket, 8192)) {
-            $P = explode(":", $out);
-            $PacketName = $P[0];
-            $R = str_replace($PacketName . ":", "", $out);
-            echo str_replace($PacketName . ":", "", $out);
+        $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+        if ($socket === false) {
+            throw new RuntimeException('Could not create MUS socket: ' . socket_strerror(socket_last_error()));
         }
 
-        socket_close($Socket);
+        socket_set_option($socket, SOL_SOCKET, SO_SNDTIMEO, ['sec' => 3, 'usec' => 0]);
+        socket_set_option($socket, SOL_SOCKET, SO_RCVTIMEO, ['sec' => 3, 'usec' => 0]);
 
-        return ($return)? $R : true;
+        if (@socket_connect($socket, $this->Host, $this->Port) === false) {
+            $error = socket_last_error($socket);
+            socket_close($socket);
+            throw new RuntimeException('Could not connect to MUS server at ' . $this->Host . ':' . $this->Port . ' - ' . socket_strerror($error));
+        }
+
+        return $socket;
+    }
+
+    public function Ready($C, $P = null)
+    {
+        $packet = $C . ':';
+
+        if ($P !== null) {
+            $values = [];
+            foreach ($P as $V) {
+                $values[] = $V;
+            }
+            $packet .= implode('|', $values);
+        }
+
+        return $this->Send($packet);
+    }
+
+    public function Send($S, $return = false)
+    {
+        $socket = $this->Con();
+
+        $length = strlen($S);
+        $written = 0;
+        while ($written < $length) {
+            $result = socket_write($socket, substr($S, $written));
+            if ($result === false) {
+                $error = socket_last_error($socket);
+                socket_close($socket);
+                throw new RuntimeException('Could not send data to MUS server: ' . socket_strerror($error));
+            }
+            $written += $result;
+        }
+
+        $response = '';
+        while (($out = @socket_read($socket, 8192)) !== false && $out !== '') {
+            $response .= $out;
+            if (strlen($out) < 8192) {
+                break;
+            }
+        }
+
+        socket_close($socket);
+
+        if ($response !== '') {
+            $parts = explode(':', $response, 2);
+            $payload = isset($parts[1]) ? $parts[1] : $response;
+            if (!$return) {
+                echo $payload;
+            }
+            return $return ? $payload : true;
+        }
+
+        return $return ? null : true;
     }
 }
