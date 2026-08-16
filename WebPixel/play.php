76398dce7a0b7104f2ec581c605857e577cd52a7
TrashrProject <77882564+TrashrProject@users.noreply.github.com>
2026-08-16 14:12:04 +0200

    Bump WhatsApp avatar header cache

--- WebPixel/play.php
+++ WebPixel/play.php
@@ -88,7 +88,7 @@ $localhostShim = <<<'HTML'
 <link rel="stylesheet" href="/WebPixel/app/View/Directory/Client/websockets/ws_overlays/Phones/iPhone/resources/css/phone-whatsapp-header-v2.css?v=2">
 <script src="/WebPixel/app/View/Directory/Client/websockets/ws_overlays/Phones/iPhone/resources/js/phone-presence-toast.js?v=1" defer></script>
 <script src="/WebPixel/app/View/Directory/Client/websockets/ws_overlays/Phones/iPhone/resources/js/phone-whatsapp-send-v2.js?v=4" defer></script>
-<script src="/WebPixel/app/View/Directory/Client/websockets/ws_overlays/Phones/iPhone/resources/js/phone-whatsapp-header-v2.js?v=2" defer></script>
+<script src="/WebPixel/app/View/Directory/Client/websockets/ws_overlays/Phones/iPhone/resources/js/phone-whatsapp-header-v2.js?v=4" defer></script>
 <script>
 window.swfobject = window.swfobject || { embedSWF: function () {} };
 console.info('[RDP] RP shell + local Nitro boot active');
