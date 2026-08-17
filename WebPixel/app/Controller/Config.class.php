58f37e3546be07bfeca9d4366f8738a48a07e0f0
TrashrProject <77882564+TrashrProject@users.noreply.github.com>
2026-08-17 16:02:03 +0200

    Use ParadiseRP production branding and URLs

--- WebPixel/app/Controller/Config.class.php
+++ WebPixel/app/Controller/Config.class.php
@@ -10,9 +10,9 @@ class Config
     protected static $DBPass = '';
 
     ## Web Configuration ##
-    public static $WName = "Velora RP";
+    public static $WName = "ParadiseRP";
     public static $URL = "https://paradiserp.fr";
-public static $WEB_DY = "https://paradiserp.fr/Dynamics";
+    public static $WEB_DY = "https://paradiserp.fr/Dynamics";
     public static $SessionName = "PZ_lOGIN";
     public static $MaxUsers = 4;
     public static $_MANT = false;
@@ -22,12 +22,13 @@ public static $WEB_DY = "https://paradiserp.fr/Dynamics";
     public static $SWF = "https://paradiserp.fr/swf_pz/v5-0-2/";
 
     ## Client & Socket Configuration ##
+    // Internal server services stay on loopback; browser-facing endpoints use paradiserp.fr.
     public static $TCP = "127.0.0.1";
     public static $TCP_PORT = "2021";
     public static $WS_SERVER = "127.0.0.1";
     public static $WS_PORT = "2087";
 
-    ## External services disabled for localhost ##
+    ## External services ##
     public static $SandBox = true;
     public static $S_PAYPAL_API = "";
     public static $S_PAYPAL_SECRET = "";
@@ -45,4 +46,3 @@ public static $WEB_DY = "https://paradiserp.fr/Dynamics";
     public static $FB_ASSOC_API_REDIRECT = "https://paradiserp.fr/fb_assoc";
     public static $FB_API_REQUEST = "";
 }
-
