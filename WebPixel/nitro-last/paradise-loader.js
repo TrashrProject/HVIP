2f35d87ea1457284b84d20db968ab58772e6bce0
TrashrProject <77882564+TrashrProject@users.noreply.github.com>
2026-08-18 17:21:25 +0200

    Load ParadiseRP HUD v6

--- WebPixel/nitro-last/paradise-loader.js
+++ WebPixel/nitro-last/paradise-loader.js
@@ -1,7 +1,7 @@
 (() => {
   'use strict';
 
-  const HUD_SRC = './paradise-rp-hud.js?v=5';
+  const HUD_SRC = './paradise-rp-hud.js?v=6';
   const MAX_BLOCK_MS = 2600;
   const HARD_KILL_MS = 4200;
 
