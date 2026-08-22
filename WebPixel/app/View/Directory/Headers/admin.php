2061ccccd0fd6406c25cd0a95dd0d6d810a76341
TrashrProject <77882564+TrashrProject@users.noreply.github.com>
2026-08-22 17:27:23 +0200

    fix(admin): load overlay visibility hotfix

--- WebPixel/app/View/Directory/Headers/admin.php
+++ WebPixel/app/View/Directory/Headers/admin.php
@@ -14,5 +14,6 @@ if (!isset($PCC)) { http_response_code(500); exit; }
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&family=Ubuntu:wght@700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" crossorigin="anonymous">
     <link rel="stylesheet" href="<?php echo CSS; ?>/admin-control-center.css?v=<?php echo rawurlencode(Config::$V); ?>">
+    <link rel="stylesheet" href="<?php echo CSS; ?>/admin-control-center-hotfix.css?v=<?php echo time(); ?>">
 </head>
 <body class="pcc-body">
