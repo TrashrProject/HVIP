db1be752234f2b34855cddc1733e59eda6958947
TrashrProject <77882564+TrashrProject@users.noreply.github.com>
2026-08-22 17:44:20 +0200

    PCC V3: add dedicated admin document footer

---
+++ WebPixel/app/View/Directory/Footers/admin.php
@@ -0,0 +1,9 @@
+<script>
+window.PCC = <?php echo json_encode(array(
+    'searchUrl' => Config::$URL . '/admin.php?ajax=global-search&q=',
+    'baseUrl' => Config::$URL
+), JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE); ?>;
+</script>
+<script src="<?php echo JS; ?>/admin-control-center-v3.js?v=<?php echo rawurlencode(Config::$V); ?>"></script>
+</body>
+</html>
