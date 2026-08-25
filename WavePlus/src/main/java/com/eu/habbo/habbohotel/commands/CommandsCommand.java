0f1df2c01d49ab6c13c2bfe94320b138bae1c731
TrashrProject <77882564+TrashrProject@users.noreply.github.com>
2026-08-25 20:47:05 +0200

    revert(commands): restaure affichage basique Arcturus

--- WavePlus/src/main/java/com/eu/habbo/habbohotel/commands/CommandsCommand.java
+++ WavePlus/src/main/java/com/eu/habbo/habbohotel/commands/CommandsCommand.java
@@ -2,7 +2,7 @@ package com.eu.habbo.habbohotel.commands;
 
 import com.eu.habbo.Emulator;
 import com.eu.habbo.habbohotel.gameclients.GameClient;
-import com.eu.habbo.messages.outgoing.generic.CommandsWindowComposer;
+import com.eu.habbo.messages.outgoing.generic.alerts.MessagesForYouComposer;
 
 import java.util.List;
 
@@ -14,9 +14,20 @@ public class CommandsCommand extends Command {
     @Override
     public boolean handle(GameClient gameClient, String[] params) throws Exception {
         List<Command> commands = Emulator.getGameEnvironment().getCommandHandler().getCommandsForRank(gameClient.getHabbo().getHabboInfo().getRank().getId());
-        for (CommandsWindowComposer composer : CommandsWindowComposer.createResponses(commands)) {
-            gameClient.sendResponse(composer);
+
+        StringBuilder message = new StringBuilder(
+                Emulator.getTexts().getValue("commands.generic.cmd_commands.text", "Vos commandes")
+        );
+        message.append(" (").append(commands.size()).append("):\r\n");
+
+        for (Command command : commands) {
+            message.append(Emulator.getTexts().getValue(
+                    "commands.description." + command.permission,
+                    "commands.description." + command.permission
+            )).append("\r");
         }
+
+        gameClient.sendResponse(new MessagesForYouComposer(new String[]{message.toString()}));
 
         return true;
     }
