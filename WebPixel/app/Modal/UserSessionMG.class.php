2695701920cf02ef74bd72af9f282c8b191d7ac5
TrashrProject <77882564+TrashrProject@users.noreply.github.com>
2026-08-27 17:03:18 +0200

    chore(wave): remove legacy RDP branding from user session manager

--- WebPixel/app/Modal/UserSessionMG.class.php
+++ WebPixel/app/Modal/UserSessionMG.class.php
@@ -1,11 +1,6 @@
 <?php
 /**
- * PixelZone by RDP Services, Emulated by Retro Development Server.
- * The use of this program is restricted to clients and owners of RDP Services.
- * Any unauthorized use of this code it'll end up on deletion of the program.
- * Developers P3x & Jeihden.
- * Copyrights © 2020
- * Last Modified: $file.lastModefied
+ * ParadiseRP / WavePlus user session and authentication manager.
  */
 
 class UserSessionMG
@@ -277,7 +272,6 @@ class UserSessionMG
             return false;
         endif;
     }
-
     // Mach old or new password encryption
     public function MatchPass($U, $P){
         $stmt = mysqli_prepare($this->DB->Con(), "SELECT password FROM users WHERE username = ? LIMIT 1");
