82913f56142264a71eeb18edfae6bdea2906ff9e
TrashrProject <77882564+TrashrProject@users.noreply.github.com>
2026-09-03 14:03:13 +0200

    feat(commands): add buildmode command

---
+++ WavePlus/src/main/java/com/eu/habbo/habbohotel/commands/BuildModeCommand.java
@@ -0,0 +1,27 @@
+package com.eu.habbo.habbohotel.commands;
+
+import com.eu.habbo.habbohotel.gameclients.GameClient;
+import com.eu.habbo.habbohotel.rooms.Room;
+import com.eu.habbo.habbohotel.rooms.RoomChatMessageBubbles;
+
+public class BuildModeCommand extends Command {
+    public BuildModeCommand() {
+        super("cmd_buildmode", new String[]{"buildmode"});
+    }
+
+    @Override
+    public boolean handle(GameClient gameClient, String[] params) throws Exception {
+        Room room = gameClient.getHabbo().getHabboInfo().getCurrentRoom();
+        if (room == null || !room.hasRights(gameClient.getHabbo())) {
+            return false;
+        }
+
+        boolean enabled = ParadiseBuildState.toggleBuildMode(gameClient.getHabbo());
+        if (enabled) {
+            gameClient.getHabbo().whisper("BuildMode activé : les mobis déplacés ou posés sont forcés en Z = 0.", RoomChatMessageBubbles.ALERT);
+        } else {
+            gameClient.getHabbo().whisper("BuildMode désactivé.", RoomChatMessageBubbles.ALERT);
+        }
+        return true;
+    }
+}
