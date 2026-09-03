1762cd4b6be6a7dd29c15f11d2ea16547689d741
TrashrProject <77882564+TrashrProject@users.noreply.github.com>
2026-09-03 16:15:02 +0200

    feat(build): honor noitemfloor blocked-tile mode

--- WavePlus/src/main/java/com/eu/habbo/messages/incoming/floorplaneditor/FloorPlanEditorRequestBlockedTilesEvent.java
+++ WavePlus/src/main/java/com/eu/habbo/messages/incoming/floorplaneditor/FloorPlanEditorRequestBlockedTilesEvent.java
@@ -1,14 +1,18 @@
 package com.eu.habbo.messages.incoming.floorplaneditor;
 
+import com.eu.habbo.habbohotel.commands.NoItemFloorCommand;
+import com.eu.habbo.habbohotel.rooms.Room;
 import com.eu.habbo.messages.incoming.MessageHandler;
 import com.eu.habbo.messages.outgoing.floorplaneditor.FloorPlanEditorBlockedTilesComposer;
 
 public class FloorPlanEditorRequestBlockedTilesEvent extends MessageHandler {
     @Override
     public void handle() throws Exception {
-        if (this.client.getHabbo().getHabboInfo().getCurrentRoom() == null)
+        Room room = this.client.getHabbo().getHabboInfo().getCurrentRoom();
+        if (room == null)
             return;
 
-        this.client.sendResponse(new FloorPlanEditorBlockedTilesComposer(this.client.getHabbo().getHabboInfo().getCurrentRoom()));
+        boolean noItemFloor = NoItemFloorCommand.isEnabled(this.client.getHabbo(), room);
+        this.client.sendResponse(new FloorPlanEditorBlockedTilesComposer(room, noItemFloor));
     }
 }
