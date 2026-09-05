6b79dd756d8c82e3d8f286c3bfdd210e7293035a
Unio844 <retrodragonballae@gmail.com>
2026-09-05 18:51:13 +0200

    feat(restaurants): register restaurant commands

---
+++ WaveRP-Plugin/src/main/java/io/github/brenoepics/roleplay/utilities/RestaurantCommandRegistrar.java
@@ -0,0 +1,50 @@
+package io.github.brenoepics.roleplay.utilities;
+
+import com.eu.habbo.Emulator;
+import com.eu.habbo.habbohotel.commands.Command;
+import com.eu.habbo.habbohotel.commands.CommandHandler;
+import io.github.brenoepics.roleplay.commands.jobs.restaurant.RestaurantCommand;
+import io.github.brenoepics.roleplay.commands.jobs.restaurant.RestaurantCommand.Action;
+import io.github.brenoepics.roleplay.features.job.JobPermissions;
+import io.github.brenoepics.roleplay.features.user.CheckDatabase;
+
+public final class RestaurantCommandRegistrar {
+
+  private RestaurantCommandRegistrar() {
+  }
+
+  public static void register() {
+    registerCommand(new RestaurantCommand("cmd_restaurant_menu", new String[]{"menu"},
+        Action.MENU, JobPermissions.RESTAURANT_MENU), new String[]{"menu"},
+        ":menu - Affiche la carte du restaurant.");
+
+    registerCommand(new RestaurantCommand("cmd_restaurant_take_order",
+        new String[]{"prendrecommande"}, Action.TAKE_ORDER, JobPermissions.RESTAURANT_ORDER),
+        new String[]{"prendrecommande"},
+        ":prendrecommande <pseudo> - Prend la commande d'un client proche.");
+
+    registerCommand(new RestaurantCommand("cmd_restaurant_prepare", new String[]{"preparer"},
+        Action.PREPARE, JobPermissions.RESTAURANT_PREPARE), new String[]{"preparer"},
+        ":preparer <plat> - Prépare un produit de la carte.");
+
+    registerCommand(new RestaurantCommand("cmd_restaurant_serve", new String[]{"servir"},
+        Action.SERVE, JobPermissions.RESTAURANT_SERVE), new String[]{"servir"},
+        ":servir <pseudo> - Sert le plat préparé au client.");
+
+    registerCommand(new RestaurantCommand("cmd_restaurant_bill", new String[]{"addition"},
+        Action.BILL, JobPermissions.RESTAURANT_BILL), new String[]{"addition"},
+        ":addition <pseudo> - Envoie l'addition au client.");
+
+    registerCommand(new RestaurantCommand("cmd_restaurant_cash", new String[]{"encaisser"},
+        Action.CASH, JobPermissions.RESTAURANT_CASH), new String[]{"encaisser"},
+        ":encaisser <pseudo> - Encaisse l'addition et crédite la caisse du restaurant.");
+  }
+
+  private static void registerCommand(Command command, String[] keys, String description) {
+    Emulator.getConfig().register("commands." + command.permission + ".keys", String.join(";", keys));
+    Emulator.getTexts().register("commands.description." + command.permission, description);
+    Emulator.getTexts().update("commands.description." + command.permission, description);
+    CommandHandler.addCommand(command);
+    CheckDatabase.registerPermission(command.permission, CheckDatabase.PermissionState.ALLOWED);
+  }
+}
