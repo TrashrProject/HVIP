53fbb11dbaccf8437b38bc2f3e840f4dc16bdd68
Unio844 <retrodragonballae@gmail.com>
2026-09-05 17:46:52 +0200

    Support offline users in wanted list

--- WaveRP-Plugin/src/main/java/io/github/brenoepics/roleplay/communication/packets/emulator/outgoing/WantedListComposer.java
+++ WaveRP-Plugin/src/main/java/io/github/brenoepics/roleplay/communication/packets/emulator/outgoing/WantedListComposer.java
@@ -1,51 +1,60 @@
 package io.github.brenoepics.roleplay.communication.packets.emulator.outgoing;
 
-import com.eu.habbo.habbohotel.users.Habbo;
 import com.eu.habbo.messages.ServerMessage;
 import com.eu.habbo.messages.outgoing.MessageComposer;
-import io.github.brenoepics.roleplay.features.crime.wantedlist.CriminalRecord;
 import java.sql.Timestamp;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 
 public class WantedListComposer extends MessageComposer {
 
-  private final Map<Habbo, List<CriminalRecord>> onlineCrimes;
+  private final List<WantedEntry> wantedEntries;
   private final DateTimeFormatter dateTimeFormatter;
 
-  public WantedListComposer(Map<Habbo, List<CriminalRecord>> onlineCrimes,
+  public WantedListComposer(List<WantedEntry> wantedEntries,
       DateTimeFormatter dateTimeFormatter) {
-    this.onlineCrimes = onlineCrimes;
+    this.wantedEntries = wantedEntries;
     this.dateTimeFormatter = dateTimeFormatter;
   }
 
   @Override
   protected ServerMessage composeInternal() {
     this.response.init(6003);
+    this.response.appendInt(this.wantedEntries.size());
 
-    this.response.appendInt(this.onlineCrimes.size());
-
-    for (Map.Entry<Habbo, List<CriminalRecord>> entry : this.onlineCrimes.entrySet()) {
-      String username = entry.getKey().getHabboInfo().getUsername();
-      Optional<Timestamp> end = io.github.brenoepics.roleplay.RolePlay.getWantedManager()
-          .getUserWantedEndTimestamp(entry.getValue());
-      int stars = io.github.brenoepics.roleplay.RolePlay.getWantedManager()
-          .getUserWantedStars(entry.getValue());
-
-      this.response.appendString(username);
-      this.response.appendString(entry.getKey().getHabboInfo().getLook());
-      this.response.appendInt(stars);
-
-      if (end.isPresent()) {
-        String readableDateTime = end.get().toLocalDateTime().format(dateTimeFormatter);
-        this.response.appendString(readableDateTime);
-      } else {
-        this.response.appendString(""); // or some default value
-      }
+    for (WantedEntry entry : this.wantedEntries) {
+      this.response.appendString(entry.username);
+      this.response.appendString(entry.look == null ? "" : entry.look);
+      this.response.appendInt(Math.min(entry.stars, 5));
+      this.response.appendString(entry.endTime == null ? ""
+          : entry.endTime.toLocalDateTime().format(dateTimeFormatter));
     }
 
     return this.response;
+  }
+
+  public static class WantedEntry {
+    private final int userId;
+    private final String username;
+    private final String look;
+    private int stars;
+    private Timestamp endTime;
+
+    public WantedEntry(int userId, String username, String look) {
+      this.userId = userId;
+      this.username = username;
+      this.look = look;
+    }
+
+    public int getUserId() {
+      return userId;
+    }
+
+    public void addCrime(int crimeStars, Timestamp crimeEndTime) {
+      this.stars = Math.min(5, this.stars + Math.max(0, crimeStars));
+      if (crimeEndTime != null && (this.endTime == null || crimeEndTime.after(this.endTime))) {
+        this.endTime = crimeEndTime;
+      }
+    }
   }
 }
