e39cb7f1d20d0eefa253f96fda4be0fa4f5ffbd5
TrashrProject <leseurnath@gmail.com>
2026-09-05 15:40:47 -0700

    fix(trash): recognize French trash furniture classnames

--- migrations/20260906_enable_all_trash_bins.sql
+++ migrations/20260906_enable_all_trash_bins.sql
@@ -9,7 +9,7 @@ UPDATE items_base
 SET interaction_type = 'nahabbo_rp_trashbin',
     interaction_modes_count = GREATEST(COALESCE(interaction_modes_count, 0), 2)
 WHERE
-    LOWER(COALESCE(item_name, '')) REGEXP '(^|[_ -])(trash|trashcan|garbage|garbagecan|dumpster|dustbin|rubbish|waste|litter|recycle|recycling|bin)([_ -]|[0-9]|$)'
+    LOWER(COALESCE(item_name, '')) REGEXP '(^|[_ -])(poubelle|poubelles|corbeille|corbeilles|ordure|ordures|dechet|dechets|trash|trashcan|garbage|garbagecan|dumpster|dustbin|rubbish|waste|litter|recycle|recycling|bin)([_ -]|[0-9]|$)'
     OR LOWER(COALESCE(public_name, '')) REGEXP '(^|[ -])(poubelle|poubelles|corbeille|corbeilles|ordure|ordures|dechet|dechets|trash|garbage|dumpster|dustbin|rubbish|waste|recycle|recycling|bin)([ -]|$)';
 
 -- Cas compacts frequents dans les classnames Habbo : bin1, city_bin, waste_bin, etc.
